6b1d1dd8159638cca556944eddb90e18468f169d
koking111 <ugothit111@gmail.com>
2024-10-27 11:22:58 -0400

    delim snippet was a dumb idea

--- nvim/lua/shaleen/plugins/luasnip/tex/delims.lua
+++ nvim/lua/shaleen/plugins/luasnip/tex/delims.lua
@@ -6,28 +6,28 @@ local fmta = require("luasnip.extras.fmt").fmta
 
 local in_mathzone = require("shaleen.plugins.luasnip.tex.utils").in_mathzone
 
-return {
-  s(
-    { trig = "(", snippetType = "autosnippet", dscr = "Expands '(' into '\\left(\\right)'" },
-    fmta("\\left( <> \\right", {
-      i(1),
-    }),
-    { condition = in_mathzone }
-  ),
-
-  s(
-    { trig = "[", snippetType = "autosnippet", dscr = "Expands '[' into '\\left[\\right]'" },
-    fmta("\\left[ <> \\right", {
-      i(1),
-    }),
-    { condition = in_mathzone } -- `condition` option passed in the snippet `opts` table
-  ),
-
-  s(
-    { trig = "\\{", snippetType = "autosnippet", dscr = "Expands '\\{' into '\\left{\\right}'" },
-    fmta("\\left\\{ <> \\right\\", {
-      i(1),
-    }),
-    { condition = in_mathzone } -- `condition` option passed in the snippet `opts` table
-  ),
-}
+-- return {
+--   s(
+--     { trig = "(", snippetType = "autosnippet", dscr = "Expands '(' into '\\left(\\right)'" },
+--     fmta("\\left( <> \\right", {
+--       i(1),
+--     }),
+--     { condition = in_mathzone }
+--   ),
+--
+--   s(
+--     { trig = "[", snippetType = "autosnippet", dscr = "Expands '[' into '\\left[\\right]'" },
+--     fmta("\\left[ <> \\right", {
+--       i(1),
+--     }),
+--     { condition = in_mathzone } -- `condition` option passed in the snippet `opts` table
+--   ),
+--
+--   s(
+--     { trig = "\\{", snippetType = "autosnippet", dscr = "Expands '\\{' into '\\left{\\right}'" },
+--     fmta("\\left\\{ <> \\right\\", {
+--       i(1),
+--     }),
+--     { condition = in_mathzone } -- `condition` option passed in the snippet `opts` table
+--   ),
+-- }
