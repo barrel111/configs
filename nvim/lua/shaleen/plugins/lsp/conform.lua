d0923aaee04098699fd58db44d04bed081a30a61
shaleen <ugothit111@gmail.com>
2026-06-23 09:56:08 +0545

    apply typst sembr script before tinymist fmt

--- nvim/lua/shaleen/plugins/lsp/conform.lua
+++ nvim/lua/shaleen/plugins/lsp/conform.lua
@@ -4,6 +4,12 @@ if not setup then
 end
 
 conform.setup({
+  formatters = {
+    typst_sembr = {
+      command = vim.fn.expand("~/scripts/typst-sembr.sh"),
+      stdin = true,
+    },
+  },
   formatters_by_ft = {
     -- elm = { "elm_format" },
     -- go = { "gofmt" },
@@ -11,9 +17,18 @@ conform.setup({
     -- lua = { "stylua" },
     -- rust = { "rustfmt" },
     ocaml = { "ocamlformat" },
+    typst = { "typst_sembr" },
   },
-  format_on_save = {
-    timeout_ms = 500,
-    lsp_format = "fallback",
-  }
+})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  callback = function(args)
+    local view = vim.fn.winsaveview()
+    conform.format({
+      bufnr = args.buf,
+      timeout_ms = 500,
+      lsp_format = (vim.bo[args.buf].filetype == "typst") and "first" or "fallback",
+    })
+    vim.fn.winrestview(view)
+  end,
 })
